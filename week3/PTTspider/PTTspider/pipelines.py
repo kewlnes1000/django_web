20ccb7a082a537b1452e1aa82bdee1abbb2cf50e
Tai-Siang <kewlnes2000@gmail.com>
2019-04-06 18:08:34 +0800

    修改pipelines

--- week3/PTTspider/PTTspider/pipelines.py
+++ week3/PTTspider/PTTspider/pipelines.py
@@ -8,7 +8,7 @@ import string
 import scrapy
 from scrapy.exceptions import DropItem
 from scrapy.pipelines.files import FilesPipeline
-
+from week3.models import PTTspider
 
 class PttspiderPipeline(FilesPipeline):
     def get_media_requests(self, item, info):
@@ -16,21 +16,16 @@ class PttspiderPipeline(FilesPipeline):
             yield scrapy.Request(file_url, meta = {'item': item})
 
     def file_path(self, request, response = None, info = None):
-        # print('in file_path')
-
         item = request.meta['item']
         #取得網址最後一個字串做為檔名
         name = request.url.split('/')[-1]
-        # print("%s/%s"%(item['title'], name))
         return "spider_images/%s/%s"%(item['title'], name)
     
     def item_completed(self, results, item, info):
-        # print('in item_completed')
         file_paths = [x['path'] for ok, x in results if ok]
         if not file_paths:
             raise DropItem("Item contains no files")
         item['file_urls'] = file_paths
-        # print(item['file_urls'])
         item.save()
         return item
 
